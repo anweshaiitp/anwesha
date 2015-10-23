df4937d732ac681aa473113c61a7542a65a8aaaa
guptaaditya13 <guptaaditya.13@gmail.com>
2015-10-23 16:37:33 +0530

    Made directory structure and control flow

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php 
+echo "Hello. going to dump server<br>";
+var_dump($_SERVER);
+echo "<br> going to dump request<br>";
+var_dump($_REQUEST)
+ ?>
---
+++ routes.php
@@ -0,0 +1,34 @@
+<?php 
+/**
+* New request lands in this class. Sfter that it is routed accordingly to the respective controller.
+* Also provides basic functions for loading models.
+* Also provides basic methods for HTTP responses nad redirects.
+*/
+class Routing
+{
+	
+	function __construct()
+	{
+		return null;
+	}
+
+	public function Redirect($url)
+	{
+		return null;
+	}
+
+}
+
+$url = $_SERVER['PATH_INFO'];
+if (preg_match('/^events$/', $url, $match)){
+	require ('/controller/events.php');
+} elseif (preg_match('/^events/([A-Za-z]+)$/', $url, $match)) {
+	require ('/controller/club.php');
+} elseif (preg_match('/^events/([A-Za-z]+)/([A-Za-z0-9])$/', $url, $match)) {
+	require ('/controller/eventDetail.php');
+} else {
+	die('invalid url');
+} 
+
+
+?>
