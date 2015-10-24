03f0878ce06c3b66fa9d88025d2dccfe10a5a74e
guptaaditya13 <guptaaditya.13@gmail.com>
2015-10-24 23:49:54 +0530

    debugged .htaccess

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # anwesha
 website for anwesha 2016
+
+place the anwesha folder in your server's root direcory.
--- index.php
+++ index.php
@@ -1,6 +1,36 @@
 <?php 
-echo "Hello. going to dump server<br>";
-var_dump($_SERVER);
-echo "<br> going to dump request<br>";
-var_dump($_REQUEST)
- ?>
+/**
+* New request lands in this class. Sfter that it is routed accordingly to the respective controller.
+* Also provides basic functions for loading models.
+* Also provides basic methods for HTTP responses nad redirects.
+*/
+class Routing
+{
+	
+	function __construct()
+	{
+		return null;
+	}
+
+	public function Redirect($url)
+	{
+		return null;
+	}
+
+}
+// echo "I am here";
+$url = $_SERVER['PATH_INFO'];
+// echo $url; output = /hey for localhost/anwesha/hey
+
+if ( preg_match('@^events$@', $url, $match ) ) {
+	require ('/controller/events.php');
+} elseif (preg_match('@^events/([A-Za-z]+)$@', $url, $match)) {
+	require ('/controller/club.php');
+} elseif (preg_match('@^events/([A-Za-z]+)/([A-Za-z0-9])$@', $url, $match)) {
+	require ('/controller/eventDetail.php');
+} else {
+	die('invalid url');
+} 
+
+
+?>
