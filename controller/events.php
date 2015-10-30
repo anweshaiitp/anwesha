7d487ae2c333ecb4bc354e79e6d87f6cbcf897cd
guptaaditya13 <guptaaditya.13@gmail.com>
2015-10-31 00:59:19 +0530

    generic routing

--- controller/events.php
+++ controller/events.php
@@ -5,7 +5,7 @@ require('dbConnection.php');
  * will store connection link
  */
 $conn = mysqli_connect(SERVER_ADDRESS,USER_NAME,PASSWORD,DATABASE);
-$events = Event::getAllEventCategory($conn);
+$events = Events::getMainEvents($conn);
 mysqli_close($conn);
 echo json_encode($events);
 ?>
--- index.php
+++ index.php
@@ -18,19 +18,17 @@ class Routing
 	}
 
 }
-
 // set this to path where the project is located
-$root_uri = "/project";
-$matcher = "@^" . "$root_uri/anwesha/";
 $url = $_SERVER['REQUEST_URI'];
-
-if ( preg_match($matcher . 'events$@', $url, $match ) ) {
+preg_match('@(.*)index.php$@', $_SERVER['PHP_SELF'], $mat );
+$base = '@^'. $mat[1] ;
+if ( preg_match($base .'events$@', $url, $match ) ) {
 	require ('controller/events.php');
-} elseif (preg_match($matcher . 'events/([A-Za-z]+)$@', $url, $match)) {
+} elseif (preg_match($base . 'events/([A-Za-z]+)$@', $url, $match)) {
 	require ('controller/getSubEvents.php');
-} elseif (preg_match($matcher . 'events/([A-Za-z]+)/([A-Za-z0-9])$@', $url, $match)) {
+} elseif (preg_match($base . 'events/([A-Za-z]+)/([A-Za-z0-9])$@', $url, $match)) {
 	require ('controller/eventDetail.php');
-} elseif (preg_match($matcher . '$@', $url)) {
+} elseif (preg_match($base . '$@', $url)) {
 	require ('view/index.html');
 } else {
 	die('invalid url');
