fe7a156122b1938a75f7729406328547a78eb83f
Sumit Asthana <asthana.sumit23@gmail.com>
2015-12-01 00:41:45 +0530

    Uncommented jquery in regfrom

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ if (preg_match($base . '$@', $url)) {
 	require ('controller/verifyEmail.php');
 } elseif (preg_match($base . 'gallery.html@', $url)) {
 	require ('gallery.html');
-} elseif (preg_match($base . 'campusambassador', $url)) {
+} elseif (preg_match($base . 'campusambassador@', $url)) {
 	require ('campusambassador/campusambassador.php');
 } elseif (preg_match($base . 'registration@', $url)) {
 	require ('registration.html');
