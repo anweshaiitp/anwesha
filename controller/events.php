3b01bd6b65efb67617f7405c9ab734a501b4b277
guptaaditya13 <guptaaditya.13@gmail.com>
2016-01-09 14:36:53 +0530

    Added cache reload on events

--- controller/events.php
+++ controller/events.php
@@ -18,6 +18,11 @@ $events = Events::getMainEvents($conn);
 mysqli_close($conn);
 
 header('Content-type: application/json');
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate"); // HTTP/1.1
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache"); // HTTP/1.0
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 ob_start('ob_gzhandler');
 
 echo json_encode($events);
