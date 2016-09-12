cbc6d376e1da1c184cc6276b10c678efe431ce18
scopeinfinity <gagan1kumar@ymail.com>
2016-09-12 16:59:13 +0530

    Moved files into proper structure and removed unwanted files

--- index.php
+++ index.php
@@ -27,19 +27,19 @@ $base = '@^'. $mat[1] ;
 if (preg_match($base . '$@', $url)) {
 	require ('view/index.html');
 } elseif (preg_match($base . 'landing$@', $url)) {
-	require ('view/main.html');
+	require ('view/stars.html');
 } elseif (preg_match($base . 'register/([0-9]{4}|)$@', $url, $match)) {
 	require ('view/userRegistration.php');
 } elseif (preg_match($base . 'ca/([0-9]{4}|)$@', $url, $match)) {
 	require ('view/ca.php');
-} elseif ( preg_match($base .'cssLoader/home/?$@', $url, $match ) ) {
-	require ('controller/cssLoader.php');
-} elseif ( preg_match($base .'events/?$@', $url, $match ) ) {
-	require ('controller/events.php');
-} elseif (preg_match($base . 'allEvents/?$@', $url)) {
-	require ('controller/allEvents.php');
-} elseif (preg_match($base . 'events/([A-Za-z&-]+)/?$@', $url, $match)) {
-	require ('controller/getSubEvents.php');
+// } elseif ( preg_match($base .'cssLoader/home/?$@', $url, $match ) ) {
+// 	require ('controller/cssLoader.php');
+// } elseif ( preg_match($base .'events/?$@', $url, $match ) ) {
+	// require ('controller/events.php');
+// } elseif (preg_match($base . 'allEvents/?$@', $url)) {
+// 	require ('controller/allEvents.php');
+// } elseif (preg_match($base . 'events/([A-Za-z&-]+)/?$@', $url, $match)) {
+// 	require ('controller/getSubEvents.php');
 } elseif (preg_match($base . 'user/register/User/?$@', $url)) {
 	require ('controller/userRegistration.php');
 } elseif (preg_match($base . 'user/register/CampusAmbassador/?$@', $url)) {
@@ -48,12 +48,12 @@ if (preg_match($base . '$@', $url)) {
 	require ('controller/verifyEmail.php');
 } elseif (preg_match($base . 'verifyEmail/User/([0-9]{4})/([A-Za-z0-9]{40})/?$@', $url, $match)) {
 	require ('controller/verifyEmail.php');
-} elseif (preg_match($base . 'register/([0-9]{1,2})/?$@', $url, $match)) {
-	require ('controller/registerUser.php');
-} elseif (preg_match($base . 'register/group/([0-9]{1,2})/?$@', $url, $match)) {
-	require ('controller/registerGroup.php');
-} elseif (preg_match($base . 'login/?$@', $url)) {
-	require ('controller/loginUser.php');
+// } elseif (preg_match($base . 'register/([0-9]{1,2})/?$@', $url, $match)) {
+// 	require ('controller/registerUser.php');
+// } elseif (preg_match($base . 'register/group/([0-9]{1,2})/?$@', $url, $match)) {
+// 	require ('controller/registerGroup.php');
+// } elseif (preg_match($base . 'login/?$@', $url)) {
+// 	require ('controller/loginUser.php');
 } elseif (preg_match($base . 'changePassword/?$@', $url)) {
 	require ('controller/changePassword.php');
 } elseif (preg_match($base . 'resendEmail/([0-9]{4})/?$@', $url, $match)) {
