e13fe60f1172090b8abe7ae2e3a3fcc5662592c6
scopeinfinity <gagan1kumar@ymail.com>
2016-10-02 19:22:50 +0530

    Download csv as file

--- controller/download_records.php
+++ controller/download_records.php
@@ -1,6 +1,8 @@
 <?php 
 $passgiven = md5($match[1]);
 if(strcmp($passgiven, "7bbf03bf27ed872a1fe74d0e9f1e6e41")==0) {
+	header("Content-type: text/plain");
+	header("Content-Disposition: attachment; filename=records.csv");
 	echo shell_exec("sh export_data.sh");
 }
 else
