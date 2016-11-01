e4f16b02062df72604a8b3b867439b8f176e7d9e
Tameesh Biswas <tameeshbiswas1998@gmail.com>
2016-11-01 20:51:11 +0530

    Un-comment line1 "display errors"

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-// ini_set( "display_errors", 0); 
+ini_set( "display_errors", 0); 
 /**
 * New request lands in this class. After that it is routed accordingly to the respective controller.
 * Also provides basic functions for loading models.
