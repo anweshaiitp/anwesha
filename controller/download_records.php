21cf3ba454867b8f5a92746d44053fb91c367234
scopeinfinity <gagan1kumar@ymail.com>
2016-11-19 02:17:43 +0530

    NewPassword for admins

--- controller/download_records.php
+++ controller/download_records.php
@@ -1,6 +1,6 @@
 <?php 
 $passgiven = md5($match[1]);
-if(strcmp($passgiven, "7bbf03bf27ed872a1fe74d0e9f1e6e41")==0) {
+if(strcmp($passgiven, "7128af58fc958f0cc27e8f98b5599bf1")==0) {
 	header("Content-type: text/plain");
 	header("Content-Disposition: attachment; filename=records.csv");
 	echo shell_exec("sh export_data.sh");
