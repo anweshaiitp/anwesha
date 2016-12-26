d1a29ca92c87e64ff872c5e7646b23fd007b524f
scopeinfinity <gagan1kumar@ymail.com>
2016-12-26 07:59:48 +0000

    Payload Script modified

--- Payload/script.sh
+++ Payload/script.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 git pull origin `git rev-parse --abbrev-ref HEAD` 2>&1 | tee Payload/log.txt 
--- controller/payload.php
+++ controller/payload.php
@@ -11,20 +11,23 @@ echo "Saved";
 $conf = json_decode(file_get_contents('Payload/payload.conf'),true);
 $branchname = exec('git rev-parse --abbrev-ref HEAD');
 $allowedBranch = $conf["allowedBranch"];
+//print_r($conf);
 
 if(!in_array($branchname, $allowedBranch) ) {
-	echo "This Branch is not Allowed";
+	echo "$branchname Branch is not Allowed";
 	exit();
 }
 $MAX = $conf['maxPayloadLength'];
 $header = getallheaders();
 //Prevent Attack using logs
 if(isset($_POST['payload']) &&  $header['Content-Length']<$MAX) {
+	echo "Attemping Payload\n";
 	$data = json_decode($_POST['payload']);
 	if (json_last_error() == JSON_ERROR_NONE) {
 		file_put_contents('logs/github.txt',print_r($data,TRUE));
-		echo execute('./Payload/script.sh');			
-		echo "Attempt Payload";	
+		echo "Attempt Executing Script\n";
+		echo shell_exec('./Payload/script.sh')."\n\n";			
+		echo "Payload Exit";	
 	} else "JSON Error : ".json_last_error();
 }
 else 
