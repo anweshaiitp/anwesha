e00d07cc7c4e92d020ab440604733c90e1cbf31b
tameeshb <tameeshbiswas1998@gmail.com>
2017-01-07 00:36:01 +0530

    Set Timezone on log

--- global.php
+++ global.php
@@ -1,5 +1,6 @@
 <?php
 function alog($details) {
+	date_default_timezone_set('Asia/Calcutta');
 	$errc = file_get_contents('./logs/errc.txt');
 	$log = $errc . " | " . date('h:i:s a', time()) . " > " . $details . "\n";
 	file_put_contents('./logs/auth_errors_'.date("j.n.Y").'.txt', $log, FILE_APPEND);
