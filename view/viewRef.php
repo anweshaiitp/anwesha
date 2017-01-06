3be36a9ce3444fdd65b7672bfc0c8c57531ad99d
tameeshb <tameeshbiswas1998@gmail.com>
2017-01-06 23:54:41 +0530

    viewRef

--- view/viewRef.php
+++ view/viewRef.php
@@ -3,20 +3,26 @@
 		
 
 		<?php
+		
+require('dbConnection.php');
+$conn = mysqli_connect(SERVER_ADDRESS,USER_NAME,PASSWORD,DATABASE);
 
 			$sql = "SELECT `name`,`pId` FROM People WHERE `confirm`=1 ";
         $result = mysqli_query($conn, $sql);
         if($result){
             while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) {
-                	 $sqlN = "SELECT `name` FROM People WHERE `refcode`=$row['pId'] ";
+            		$personID=$row['pId'];
+            		$personName=$row['name'];
+                	 $sqlN = "SELECT `name` FROM People WHERE `refcode`=$personID";
         				$resultN = mysqli_query($conn, $sqlN);
         				if(mysqli_num_rows($resultN)!=0){
-        					echo "<strong><i>$row['name']</i></strong><br>";
+        					echo "<strong><i>$personName</i></strong><br>";
         				}
         			if($resultN){
             		while ($rowN = mysqli_fetch_array($resultN, MYSQL_ASSOC)) {
                 		//$rowN[''];
-        					echo "<strong><i>$rowN['name']</i></strong><br>";
+                		$refName=$rowN['name'];
+        					echo "<strong><i>$refName</i></strong><br>";
                 			
             			}
         			}
