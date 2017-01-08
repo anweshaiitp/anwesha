603415941c41c6426802fa792a3c89b70f5e52ec
tameeshb <tameeshbiswas1998@gmail.com>
2017-01-08 05:41:18 +0530

    AJAX minor exception handling

--- view/index.php
+++ view/index.php
@@ -933,6 +933,8 @@
 			<div class="loginexit"><a onclick="window.location.hash ='#';document.body.style.overflow='visible';" style="cursor: pointer">x</a></div>
 			<h2 style="font-size: 2em;" class="loginhead">Login</h2>
 			<span id="loginerror" style="color:red;padding:5px;"></span>
+			<span id="loginajaxerror" style="color:red;padding:5px;"></span>
+
 				<input class="inp loginname" name="username" type="text" value="AnweshaID"
 					onblur="if (this.value == '') {this.value = 'AnweshaID';}"
 					onfocus="if (this.value == 'AnweshaID') {this.value = '';}" />
@@ -993,6 +995,12 @@
 					});
 					var onresetpassORresendEmail = function(){$("#loginerror").empty();
 					$(".logingif").fadeIn();
+					setTimeout(function(){
+						$("#loginajaxerror").text("Please Try again");
+										$(".logingif").fadeOut();
+
+
+					},5000);
 						console.log("Clicked to "+$(this).attr("placeholder"));
 						$("#loginsubmit").fadeOut();
 						$(".loginpswd").fadeOut();
@@ -1000,7 +1008,7 @@
 						if($(this).attr("placeholder")=="resend"){
 							$(".loginemail").fadeIn();
 							$(".loginname").fadeOut();
-
+							$("[placeholder='reset']").hide();
 						var username=$(".loginemail").val();
         						console.log("resend");
 
@@ -1031,6 +1039,7 @@
 	                                		$(".loginpswd").fadeOut();
 											
 											$(".loginhead").css("color","green");
+											$("#loginajaxerror").text('');
 	                                	} else {
                                 			$(".loginhead").css("color","yellow");
 	                                	}
