c5b94dc8c5f1e8b5da56a8352a53350d34d7685e
scopeinfinity <gagan1kumar@ymail.com>
2017-01-23 23:33:06 +0530

    Updated Text in Reset

--- view/reset.php
+++ view/reset.php
@@ -6,8 +6,9 @@ $url = "now/";
 <title>Anwesha - Password Change</title>
 </head>
 <body>
+	Enter 6 to 30 digits[alpha-numeric] password<br>
 	<form action='<?php echo $url;?>' method="post">
-		<input type='text' name='newPassword' placeholder="Password" pattern="[a-zA-Z0-9]{6,30}" title="Password should be alpha number 6 to 30 digits">
+		<input type='text' name='newPassword' placeholder="New Password" pattern="[a-zA-Z0-9]{6,30}" title="Password should be alpha number 6 to 30 digits">
 		<input type='submit' value='Change Password'>
 	</form>
 </body>
