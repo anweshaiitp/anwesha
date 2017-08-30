1aa2f709ab6532fab64e0721473912fca0057fee
tameeshb <tameeshbiswas1998@gmail.com>
2017-08-31 04:03:40 +0530

    2018 FB oauth remove deprecated auth scopes

--- view/ca.php
+++ view/ca.php
@@ -57,7 +57,7 @@
 			    console.log(Lstatus);
 			    if(Lstatus.status == "connected"){
 			    	clearInterval(refreshIntervalId);
-				FB.api('/me?fields=name,first_name,birthday,email,picture.width(500).height(500)', function(response) {
+				FB.api('/me?fields=name,first_name,public_profile,user_location,user_birthday,user_about_me,birthday,email,picture.width(500).height(500)', function(response) {
 					console.log(response);
 			      console.log('Successful login for: ' + response.name);
 					$("#FB-Oauth").html("Hi! " +response.first_name+"<br> Email:"+response.email);
@@ -76,7 +76,7 @@
 				<h1>Campus Ambassador Programme</h1><br>
 				<div id="FB-Oauth">
 					Sign-Up with Facebook
-					<div class="fb-login-button" data-max-rows="1" data-size="large" data-button-type="login_with"  data-auto-logout-link="true" data-use-continue-as="true" data-scope="basic_info,email,public_profile,user_location,user_birthday,user_about_me" onlogin="auth_response_change_callback();"></div><!-- <br>
+					<div class="fb-login-button" data-max-rows="1" data-size="large" data-button-type="login_with"  data-auto-logout-link="true" data-use-continue-as="true" data-scope="email,public_profile,user_location,user_birthday,user_about_me" onlogin="auth_response_change_callback();"></div><!-- <br>
 					<a id="test">This this</a> -->
 				</div>
 			
