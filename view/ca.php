a18d5186e85547666cc5989b9de6ed21298d046f
tameeshb <tameeshbiswas1998@gmail.com>
2017-08-31 12:32:51 +0530

    2018 REgistration step 2

--- view/ca.php
+++ view/ca.php
@@ -14,15 +14,15 @@
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
 		<!--[if lte IE 8]><link rel="stylesheet" href="../assets/css/ie8.css" /><![endif]-->
 		<style>
-			#FB-Oauth{
+			#FB-Oauth,#FB-Oauth2{
 				padding: 10px;
 				    background-color: rgba(98, 153, 193, 0.76);
 				    width: 400px;
 				    margin: 30px;
 				    border-radius: 10px;
 				    left: 50%;
-				    position: absolute;
-				    transform: translateX(-50%);
+				    /*position: absolute;*/
+				    /*transform: translateX(-50%);*/
 			}
 		</style>
 	</head>
@@ -48,6 +48,7 @@
 	</script>
 	<script>
 	$(document).ready(function(){
+		var name,email,dob,pic;
 		var stat=0;
 		function auth_response_change_callback(){
 			console.log("called");
@@ -57,10 +58,30 @@
 			    console.log(Lstatus);
 			    if(Lstatus.status == "connected"){
 			    	clearInterval(refreshIntervalId);
+			    	//REST call with FB userID fetches if signedu or not.
+			    	//If not, then post request to the same for registering and validation.
 				FB.api('/me?fields=name,first_name,education,birthday,email,picture.width(500).height(500)', function(response) {
 					console.log(response);
 			      console.log('Successful login for: ' + response.name);
-					$("#FB-Oauth").html("Hi! " +response.first_name+"<br> Email:"+response.email);
+			      name = response.name;
+			      email = response.email;
+			      DOB = response.birthday;
+			      pic = response.picture;
+			      //is signed up, display, your ref code is: and listing of users, leaderboard
+			      if(name){
+				      $("input[name='name']").val(name);
+			      	  $("input[name='name']").attr('disabled','true');
+			      }
+			  	  if(email){
+			      $("input[name='email']").val(email);
+			  	  $("input[name='email']").attr('disabled','true');
+			  	  }
+			  	   if(DOB){
+			      $("input[name='DOB']").val(DOB);
+			  	  $("input[name='DOB']").attr('disabled','true');
+			  	  }
+					$("#FB-Oauth").html("Hi! " +response.first_name+" <ul class='actions'><li><a href='#signUp' class='button'>Continue to step 2</a></li></ul>");
+					$("#FB-Oauth2").html("Hi! " +response.first_name+" Complete signUp below");
 				});
 
 				}
@@ -74,11 +95,14 @@
 				<span class="logo icon "></span><h6>IIT Patna</h6>
 				<h1>Anwesha</h1><br>
 				<h1>Campus Ambassador Programme</h1><br>
+				<center>
+					
 				<div id="FB-Oauth">
 					Sign-Up with Facebook
 					<div class="fb-login-button" data-max-rows="1" data-size="large" data-button-type="login_with"  data-auto-logout-link="true" data-use-continue-as="true" data-scope="email,public_profile,user_location,user_birthday,user_about_me" onlogin="auth_response_change_callback();"></div><!-- <br>
 					<a id="test">This this</a> -->
 				</div>
+				</center>
 			
 			</div>
 
@@ -129,7 +153,7 @@
 					</header>
 					<section>
 						<header>
-							<h3>Paragraph</h3>
+							<h3>Paragraph</h3><br>
 							<p>This is the subtitle for this particular heading</p>
 						</header>
 						<p>Phasellus nisl nisl, varius id <sup>porttitor sed pellentesque</sup> ac orci. Pellentesque
@@ -154,11 +178,7 @@
 						tempus euismod. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
 						posuere cubilia.</p>
 						<hr />
-						<p>Donec consectetur vestibulum dolor et pulvinar. Etiam vel felis enim, at viverra
-						ligula. Ut porttitor sagittis lorem, quis eleifend nisi ornare vel. Praesent nec orci
-						facilisis leo magna. Cras sit amet urna eros, id egestas urna. Quisque aliquam
-						tempus euismod. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
-						posuere cubilia.</p>
+						
 					</section>
 					<section>
 						<header>
@@ -171,17 +191,7 @@
 							<li>Donec consectetur vestibulum dolor et pulvinar. Etiam vel felis enim, at viverra ligula. Ut porttitor sagittis lorem, quis eleifend nisi ornare vel.</li>
 						</ul>
 					</section>
-					<section>
-						<header>
-							<h3>Ordered List</h3>
-						</header>
-						<ol class="default">
-							<li>Donec consectetur vestibulum dolor et pulvinar. Etiam vel felis enim, at viverra ligula. Ut porttitor sagittis lorem, quis eleifend nisi ornare vel.</li>
-							<li>Donec consectetur vestibulum dolor et pulvinar. Etiam vel felis enim, at viverra ligula. Ut porttitor sagittis lorem, quis eleifend nisi ornare vel.</li>
-							<li>Donec consectetur vestibulum dolor et pulvinar. Etiam vel felis enim, at viverra ligula. Ut porttitor sagittis lorem, quis eleifend nisi ornare vel.</li>
-							<li>Donec consectetur vestibulum dolor et pulvinar. Etiam vel felis enim, at viverra ligula. Ut porttitor sagittis lorem, quis eleifend nisi ornare vel.</li>
-						</ol>
-					</section>
+					
 					<section>
 						<header>
 							<h3>Table</h3>
@@ -231,43 +241,7 @@
 							</table>
 						</div>
 					</section>
-					<section>
-						<header>
-							<h3>Form</h3>
-						</header>
-						<form method="post" action="#">
-							<div class="row">
-								<div class="6u 12u(mobilep)">
-									<label for="name">Name</label>
-									<input class="text" type="text" name="name" id="name" value="" placeholder="John Doe" />
-								</div>
-								<div class="6u 12u(mobilep)">
-									<label for="email">Email</label>
-									<input class="text" type="text" name="email" id="email" value="" placeholder="johndoe@domain.tld" />
-								</div>
-							</div>
-							<div class="row">
-								<div class="12u">
-									<label for="subject">Subject</label>
-									<input class="text" type="text" name="subject" id="subject" value="" placeholder="Enter your subject" />
-								</div>
-							</div>
-							<div class="row">
-								<div class="12u">
-									<label for="subject">Message</label>
-									<textarea name="message" id="message" placeholder="Enter your message" rows="6"></textarea>
-								</div>
-							</div>
-							<div class="row">
-								<div class="12u">
-									<ul class="actions">
-										<li><input type="submit" value="Send" /></li>
-										<li><input type="reset" value="Reset" class="alt" /></li>
-									</ul>
-								</div>
-							</div>
-						</form>
-					</section>
+					
 				</div>
 			
 
@@ -286,13 +260,18 @@
 				<div class="container 75%">
 
 					<header class="major last">
-						<h2>Questions or comments?</h2>
+						<h2>Sign-Up</h2>
+						<center>
+							<h2>Step 1</h2>
+						<div id="FB-Oauth2">
+							 Facebook
+							<div class="fb-login-button" data-max-rows="1" data-size="large" data-button-type="login_with"  data-auto-logout-link="true" data-use-continue-as="true" data-scope="email,public_profile,user_location,user_birthday,user_about_me" onlogin="auth_response_change_callback();"></div><!-- <br>
+							<a id="test">This this</a> -->
+						</div>
+						</center>
 					</header>
-
-					<p>Vitae natoque dictum etiam semper magnis enim feugiat amet curabitur tempor
-					orci penatibus. Tellus erat mauris ipsum fermentum etiam vivamus.</p>
-
-					<form method="post" action="#">
+					<form method="post" action="#" id="signUp" style="display: block">
+						<h2>Step 2</h2><br>
 						<div class="row">
 							<div class="6u 12u(mobilep)">
 								<input type="text" name="name" placeholder="Name" />
@@ -302,29 +281,73 @@
 							</div>
 						</div>
 						<div class="row">
+							<div class="6u 12u(mobilep)">
+								<input type="text" name="college" placeholder="College"  />
+							</div>
+							<div class="6u 12u(mobilep)">
+								<input type="text" name="degree" placeholder="Degree"  />
+							</div>
+						</div>
+						<div class="row">
+							<div class="6u 12u(mobilep)">
+								<input type="number" name="yearOfGraduation" placeholder="Year of Graduation" />
+							</div>
+							<div class="6u 12u(mobilep)">
+								<input type="text" name="city" placeholder="City" />
+							</div>
+						</div>
+						<div class="row">
+							<div class="6u 12u(mobilep)">
+								<input type="number" name="mobile" placeholder="Mobile(10 digit)" />
+							</div>
+							<div class="6u 12u(mobilep)">
+								<input type="text" name="DOB" placeholder="Date of Birth" />
+							</div>
+						</div>
+						<!-- gender -->
+						<div class="row">
 							<div class="12u">
-								<textarea name="message" placeholder="Message" rows="6"></textarea>
+								<textarea name="address" placeholder="Address" rows="3"></textarea>
 							</div>
 						</div>
 						<div class="row">
 							<div class="12u">
+								<textarea name="threethings" placeholder="Tell us 3 things you would do as a Campus Ambassador of Anwesha '18." rows="4"></textarea>
+							</div>
+						</div>
+						<div class="row">
+							<div class="12u">
+								<textarea name="responsibility" placeholder="Have you held any position of responsibility in your college? If yes, please explain." rows="4"></textarea>
+							</div>
+						</div>
+						<div class="row">
+							<div class="12u">
+								<textarea name="involvement" placeholder="Have you been a part of one or more previous editions of Anwesha? If yes, please explain." rows="4"></textarea>
+							</div>
+						</div>
+						
+						<div class="row">
+							<div class="12u">
+								<input type="text" name="referalcode" placeholder="Optional referal code" />
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="12u">
 								<ul class="actions">
-									<li><input type="submit" value="Send Message" /></li>
+									<li><input type="submit" value="Submit" /></li>
 								</ul>
 							</div>
 						</div>
 					</form>
 
 					<ul class="icons">
-						<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
 						<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
 						<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-						<li><a href="#" class="icon fa-github"><span class="label">Github</span></a></li>
-						<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
-					</ul>
+						</ul>
 
 					<ul class="copyright">
-						<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+						<li>Anwesha IIT Patna</li>
 					</ul>
 
 				</div>
