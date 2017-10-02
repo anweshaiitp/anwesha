200dcebe08c99f88bd89b18694f2f174181e282f
tameeshb <tameeshbiswas1998@gmail.com>
2017-10-03 03:43:00 +0530

    Auto deploy test

--- defines.php
+++ defines.php
@@ -2,6 +2,6 @@
 $ANWESHA_YEAR = 'Anwesha2k18';
 $ANWESHA_URL = 'http://anwesha.info/';
 $ANWESHA_REG_EMAIL = 'anweshaiitp@gmail.com';
-$ANWESHA_REG_CONTACT = 'Sanket Patil (9604342683)';
-$ca_leader = 'https://anwesha.info/ca/#leader';
+$ANWESHA_REG_CONTACT = 'Sanket Patil (9604342683), Milan Jolly (9867794441)';
+$ca_leader = 'http://anwesha.info/ca/#leader';
 ?>
