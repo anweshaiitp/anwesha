f720ee6ee748fda6cecd6f27b5b8094d1077208d
tameeshb <tameeshbiswas1998@gmail.com>
2017-10-04 18:10:03 +0530

    Leaderboard hide test accounts

--- controller/leaderboardback.php
+++ controller/leaderboardback.php
@@ -4,7 +4,7 @@ require('dbConnection.php');
 $conn = mysqli_connect(SERVER_ADDRESS,USER_NAME,PASSWORD,DATABASE);
 $rlist=array();
 //parent loop for getting pid and refkey
-$sql = "select name,CA.pId,(select count(*) from People P2 where P2.refCode = CA.pId and P2.confirm<>0)*50+20 as Score from CampusAmberg CA inner join People P on P.pId=CA.pId where P.iitp<>1 and P.confirm<>0 order by Score DESC,name;";
+$sql = "select name,CA.pId,(select count(*) from People P2 where P2.refCode = CA.pId and P2.confirm<>0)*50+20 as Score from CampusAmberg CA inner join People P on P.pId=CA.pId where P.iitp<>1 and P.confirm<>0 and P.name NOT LIKE 'test%' order by Score DESC,name;";
 if ($result=mysqli_query($conn,$sql))
   { 
 	while ($row=mysqli_fetch_row($result))
