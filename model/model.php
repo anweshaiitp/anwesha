723390bae6a3c4bbfb61ed7e0001fd2e6d2dc279
tameeshb <tameeshbiswas1998@gmail.com>
2017-10-26 02:28:19 +0530

    QR code generate

--- model/model.php
+++ model/model.php
@@ -320,6 +320,73 @@ class People{
     public function getGroups($id, $conn){
 
     }
+
+    public static function resize($newWidth, $targetFile, $originalFile) {
+
+        $info = getimagesize($originalFile);
+        $mime = $info['mime'];
+
+        switch ($mime) {
+            case 'image/jpeg':
+                    $image_create_func = 'imagecreatefromjpeg';
+                    $image_save_func = 'imagejpeg';
+                    $new_image_ext = 'jpg';
+                    break;
+
+            case 'image/png':
+                    $image_create_func = 'imagecreatefrompng';
+                    $image_save_func = 'imagepng';
+                    $new_image_ext = 'png';
+                    break;
+
+            case 'image/gif':
+                    $image_create_func = 'imagecreatefromgif';
+                    $image_save_func = 'imagegif';
+                    $new_image_ext = 'gif';
+                    break;
+
+            default: 
+                    throw new Exception('Unknown image type.');
+        }
+
+        $img = $image_create_func($originalFile);
+        list($width, $height) = getimagesize($originalFile);
+
+        $newHeight = ($height / $width) * $newWidth;
+        $tmp = imagecreatetruecolor($newWidth, $newHeight);
+        imagecopyresampled($tmp, $img, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+
+        if (file_exists($targetFile)) {
+                unlink($targetFile);
+        }
+        $image_save_func($tmp, "$targetFile");
+    }
+
+    public static function genQR($anwID){
+        require('resources/phpqr/qrlib.php');
+        require('defines.php');
+        
+        $tempDir = 'qr/';
+        
+        $codeContents = $anwID;
+        
+        // we need to generate filename somehow, 
+        // with md5 or with database ID used to obtains $codeContents...
+        $fileName = 'anw'.$anwID.'.png';
+        
+        $pngAbsoluteFilePath = $tempDir.$fileName;
+        $urlRelativeFilePath = 'qr/'.$fileName;
+        $ret = "";
+        // generating
+        if (!file_exists($pngAbsoluteFilePath)) {
+            QRcode::png($codeContents, $pngAbsoluteFilePath);
+            $ret = self::resize(500,$_SERVER['DOCUMENT_ROOT'].'/qr/'.$fileName,$_SERVER['DOCUMENT_ROOT'].'/qr/'.$fileName);
+        } else {
+            return[-1,409,"Already generated"];
+        }
+        $actual_link = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
+        return [1,200,$urlRelativeFilePath,$actual_link.'/'.$pngAbsoluteFilePath];
+    }
     /**
      * Registers a new user for anwesha
      * @param  string $n    name of user
