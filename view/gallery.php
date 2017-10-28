e5149a7f60d882f829a533541c093f2557dc614b
tameeshb <tameeshbiswas1998@gmail.com>
2017-10-28 07:44:12 +0530

    New Gallery

--- view/gallery.php
+++ view/gallery.php
@@ -1,5 +1,6 @@
 <?php
         require_once 'vendor/autoload.php';
+        require 'defines.php';
    // require_once 'library/facebook.php';
     //try{
 $fbAlbums = array(
@@ -12,95 +13,214 @@ $fb = new \Facebook\Facebook([
           'default_graph_version' => 'v2.10',
 ]);
 foreach ($fbAlbums as $fbAlbum) {
-	$GraphResponse = $fb->get("/$fbAlbum/photos?fields=images","$fbAppID|$fbAppSecret");
+	$GraphResponse = $fb->get("/$fbAlbum/photos?fields=images&limit=5","$fbAppID|$fbAppSecret");
 	$graphEdge = $GraphResponse->getGraphEdge();
 	//$photos2 = $photos->getField('images');
 	foreach($graphEdge as $graphNode){
 	//var_dump($photo->getField('images'))
 		foreach($graphNode->getField('images') as $photo){
 		        $images[] = $photo->getField('source');
+		        break;
 		}
+		
 	}
 }
+// var_dump($graphEdge);
 ?>
 <!doctype html>
 <html class="no-js">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Flipster Demo</title>
+    <title>Gallery</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=550, initial-scale=1">
 
     <link rel="stylesheet" href="demo.css">
-    <link rel="stylesheet" href="../dist/jquery.flipster.min.css">
+    <link rel="stylesheet" href="/assets/jquery.flipster.min.css">
 
-    <script src="jquery.min.js"></script>
-    <script src="../dist/jquery.flipster.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+    
+    <script src="/assets/jquery.flipster.min.js"></script>
+
+    <style>
+    	/*
+    	* Base Template
+    	* Combines HTML5 Boilerplate & Boostrap. Includes some basic templating.
+    	* Authored by Stephen Shaw (shshaw@gmail.com)
+    	*/
+
+    	/* @group Basics */
+
+    	*,
+    	*:before,
+    	*:after {
+    	  box-sizing: border-box;
+    	}
+
+    	html,
+    	body {
+    	  margin: 0;
+    	  padding: 0;
+    	  height: 100%;
+    	  background-image: url('https://i.ytimg.com/vi/t0kGRcd-IFY/maxresdefault.jpg');
+			background-size: cover;
+			background-repeat: no-repeat;
+		    background-attachment: fixed;
+
+    	}
+
+    	body {
+    	  color: #333333;
+    	  font-weight: 400;
+    	  font-size: 15px;
+    	  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    	  line-height: 20px;
+    	  margin: 0;
+    	  /*padding: 4em 0 4em 20%;*/
+    	}
+
+    	a {
+    	  color: #da0d25;
+    	  text-decoration: none;
+    	}
+    	a:hover,
+    	a:focus {
+    	  color: #f22840;
+    	  text-decoration: none;
+    	}
+    	p { margin: 0 0 20px; }
+
+    	h1,
+    	h2,{
+    	  margin: 10px 0;
+    	  font-weight: 500;
+    	  line-height: 40px;
+    	  color: inherit;
+    	  text-align: center;
+    	}
+    	h1 small,
+    	h2 small {
+    	  font-weight: 400;
+    	  line-height: 1;
+    	  color: #8d8d8d;
+    	}
+    	h1 { font-size: 28px; }
+    	h2 { font-size: 22px; }
+
+    	/* @end */
+
+    	.main-header {
+    	  position: fixed;
+    	  top: 0;
+    	  bottom: 0;
+    	  left: 0;
+    	  width: 20%;
+    	  background: #eee;
+    	  padding: 1em;
+    	}
+
+    	.main-header__nav {
+    	  border-top: solid 1px #ccc;
+    	  margin: 1em -1em;
+    	}
+		.flipster__nav{
+			display: none !important;
+		}
+    	.main-header__nav a {
+    	  display: block;
+    	  padding: 0.5em 1em;
+    	  border-bottom: solid 1px #ddd;
+    	}
+
+    	.main-header__nav a:hover,
+    	.main-header__nav a:focus {
+    	  color: #eee;
+    	  background: #da0d25;
+    	}
+		.flipser
+    	@media (max-width: 800px) {
+    	  body { padding: 0; }
+
+    	  .main-header {
+    	    position: relative;
+    	    width: 100%;
+    	  }
+
+    	  .main-header__nav a { display: inline-block; border: none; }
+    	}
+
+    	.intro {
+    	  padding: 4em 2em;
+    	}
+
+    	.demo {
+    	  padding: 6em 2em;
+    	}
+
+    	.demo h2 {
+    	  text-align: center;
+    	  font-size: 22px;
+    	  margin-bottom: 1em;
+    	}
+
+    	.demo .flipster {
+    	  margin: 0 -2em;
+    	}
+
+    	.code {
+    	  display: block;
+    	  background: #eee;
+    	  padding: 1em;
+    	  border-radius: 1em;
+    	  margin: 2em auto;
+    	  max-width: 30em;
+    	  font-size: 0.8em;
+    	  width: fit-content;
+    	  white-space: pre-wrap;
+    	}
+
+    	.button {
+    	  display: inline-block;
+    	  padding: 5px 10px;
+    	  margin: 0;
+    	  background-color: #da0d25;
+    	  background-position: center center;
+    	  background-repeat: no-repeat;
+    	  color: #FFF;
+    	  text-shadow: none;
+    	  vertical-align: middle;
+    	  cursor: pointer;
+    	  border: 0;
+    	  transition: all 300ms ease;
+    	}
+
+    	.button:hover,
+    	.button:focus {
+    	  color: #7a0715;
+    	  background: #f22840;
+    	}
+	@import url('https://fonts.googleapis.com/css?family=Lobster');
+    </style>
+
 </head>
 <body>
-<header class="main-header">
-    <h1>jQuery Flipster</h1>
-    <iframe src="https://ghbtns.com/github-btn.html?user=drien&repo=jquery-flipster&type=star&count=true" frameborder="0" scrolling="0" width="90px" height="20px"></iframe>
-    <a href="http://libscore.com/#$.fn.flipster" target="_blank"><img src="http://api.libscore.com/badge/$.fn.flipster.svg"></a>
-
-    <nav class="main-header__nav" role="navigation">
-        <a href="#demo-default">Default Settings (Coverflow)</a>
-        <a href="#demo-carousel">Carousel</a>
-        <a href="#demo-wheel">Wheel</a>
-        <a href="#demo-flat">Flat</a>
-    </nav>
-</header>
-
-<article id="intro" class="intro">
-    <p>jQuery Flipster is a CSS3 3D transform-based jQuery plugin built to replicate the familiar 'cover flow' effect, but also supports a variety of styles. Not only is it gorgeous to look at, Flipster is also:</p>
-
-    <ul>
-        <li><strong>Responsive</strong> Automatically centers and scales to fit the area provided.</li>
-        <li><strong>Touch Friendly</strong> Swipe between items on touch devices without interrupting scroll.</li>
-        <li><strong>Lightweight</strong> Javascript and CSS combined are only 5kb gzipped (13kb without gzip). Only dependency is jQuery.</li>
-        <li><strong>Customizable</strong> Four built in styles (coverflow, carousel, wheel and flat) with plenty of options to configure Flipster the way you want</li>
-    </ul>
-
-    <p>But don't take our word for it. Check out the demos below, or <a href="https://github.com/drien/jquery-flipster">download from GitHub</a> to try it yourself.</p>
-</article>
-
+<br><br><br><br>
+	<center><h1 style="color:#ffffff;font-size: 70px;font-family: 'Lobster', cursive;" >Gallery</h1></center>
 <article id="demo-default" class="demo">
 
-    <h2>Default Settings (Coverflow)</h2>
+    <h2></h2>
 
     <div id="coverflow">
         <ul class="flip-items">
-            <li data-flip-title="Red">
-                <img src="img/text1.gif">
-            </li>
-            <li data-flip-title="Razzmatazz" data-flip-category="Purples">
-                <img src="img/text2.gif">
-             </li>
-            <li data-flip-title="Deep Lilac" data-flip-category="Purples">
-                <img src="img/text3.gif">
-            </li>
-            <li data-flip-title="Daisy Bush" data-flip-category="Purples">
-                <img src="img/text4.gif">
-            </li>
-            <li data-flip-title="Cerulean Blue" data-flip-category="Blues">
-                <img src="img/text5.gif">
-            </li>
-            <li data-flip-title="Dodger Blue" data-flip-category="Blues">
-                <img src="img/text6.gif">
-            </li>
-            <li data-flip-title="Cyan" data-flip-category="Blues">
-                <img src="img/text7.gif">
-            </li>
-            <li data-flip-title="Robin's Egg" data-flip-category="Blues">
-                <img src="img/text8.gif">
-            </li>
-            <li data-flip-title="Deep Sea" data-flip-category="Greens">
-                <img src="img/text9.gif">
-            </li>
-            <li data-flip-title="Apple" data-flip-category="Greens">
-                <img src="img/text10.gif">
-            </li>
+            <?php
+        		foreach ($images as $imageURI) {
+        			echo "<li data-flip-title=\"\">
+			                <img src=\"".$imageURI."\" height='500'>
+			            </li>";
+        		}
+        	?>
+            
         </ul>
     </div>
 
@@ -108,128 +228,49 @@ foreach ($fbAlbums as $fbAlbum) {
     var coverflow = $("#coverflow").flipster();
 </script>
 
-<pre class="code">$("#coverflow").flipster();</pre>
-
 </article>
 
 <article id="demo-carousel" class="demo">
 
-    <h2>Flipster Carousel</h2>
+    <h2></h2>
 
     <div id="carousel">
         <ul class="flip-items">
-            <li data-flip-title="Red">
-                <img src="img/text1.gif">
-            </li>
-            <li data-flip-title="Razzmatazz" data-flip-category="Purples">
-                <img src="img/text2.gif">
-             </li>
-            <li data-flip-title="Deep Lilac" data-flip-category="Purples">
-                <img src="img/text3.gif">
-            </li>
-            <li data-flip-title="Daisy Bush" data-flip-category="Purples">
-                <img src="img/text4.gif">
-            </li>
-            <li data-flip-title="Cerulean Blue" data-flip-category="Blues">
-                <img src="img/text5.gif">
-            </li>
-            <li data-flip-title="Dodger Blue" data-flip-category="Blues">
-                <img src="img/text6.gif">
-            </li>
-            <li data-flip-title="Cyan" data-flip-category="Blues">
-                <img src="img/text7.gif">
-            </li>
-            <li data-flip-title="Robin's Egg" data-flip-category="Blues">
-                <img src="img/text8.gif">
-            </li>
-            <li data-flip-title="Deep Sea" data-flip-category="Greens">
-                <img src="img/text9.gif">
-            </li>
-            <li data-flip-title="Apple" data-flip-category="Greens">
-                <img src="img/text10.gif">
-            </li>
-            <li data-flip-title="Pistachio" data-flip-category="Greens">
-                <img src="img/text11.gif">
-            </li>
-            <li data-flip-title="Pear" data-flip-category="Greens">
-                <img src="img/text12.gif">
-            </li>
-            <li data-flip-title="Bright Sun" data-flip-category="Yellows">
-                <img src="img/text13.gif">
-            </li>
-            <li data-flip-title="Mikado" data-flip-category="Yellows">
-                <img src="img/text14.gif">
-            </li>
+        	<?php
+        		foreach ($images as $imageURI) {
+        			echo "<li data-flip-title=\"\">
+			                <img src=\"".$imageURI."\" height='300'>
+			            </li>";
+        		}
+        	?>
+            
         </ul>
     </div>
 
 <script>
     var carousel = $("#carousel").flipster({
         style: 'carousel',
-        spacing: -0.5,
-        nav: true,
-        buttons:   true,
+        spacing: 0,
     });
 </script>
-
-<pre class="code">$("#carousel").flipster({
-    style: 'carousel',
-    spacing: -0.5,
-    nav: true,
-    buttons: true,
-});</pre>
 
 </article>
 
 
 <article id="demo-wheel" class="demo">
 
-    <h2>Flipster Wheel</h2>
+    <h2></h2>
 
     <div id="wheel">
         <ul>
-            <li data-flip-title="Red">
-                <img src="img/text1.gif">
-            </li>
-            <li data-flip-title="Razzmatazz" data-flip-category="Purples">
-                <img src="img/text2.gif">
-             </li>
-            <li data-flip-title="Deep Lilac" data-flip-category="Purples">
-                <img src="img/text3.gif">
-            </li>
-            <li data-flip-title="Daisy Bush" data-flip-category="Purples">
-                <img src="img/text4.gif">
-            </li>
-            <li data-flip-title="Cerulean Blue" data-flip-category="Blues">
-                <img src="img/text5.gif">
-            </li>
-            <li data-flip-title="Dodger Blue" data-flip-category="Blues">
-                <img src="img/text6.gif">
-            </li>
-            <li data-flip-title="Cyan" data-flip-category="Blues">
-                <img src="img/text7.gif">
-            </li>
-            <li data-flip-title="Robin's Egg" data-flip-category="Blues">
-                <img src="img/text8.gif">
-            </li>
-            <li data-flip-title="Deep Sea" data-flip-category="Greens">
-                <img src="img/text9.gif">
-            </li>
-            <li data-flip-title="Apple" data-flip-category="Greens">
-                <img src="img/text10.gif">
-            </li>
-            <li data-flip-title="Pistachio" data-flip-category="Greens">
-                <img src="img/text11.gif">
-            </li>
-            <li data-flip-title="Pear" data-flip-category="Greens">
-                <img src="img/text12.gif">
-            </li>
-            <li data-flip-title="Bright Sun" data-flip-category="Yellows">
-                <img src="img/text13.gif">
-            </li>
-            <li data-flip-title="Mikado" data-flip-category="Yellows">
-                <img src="img/text14.gif">
-            </li>
+            <?php
+        		foreach ($images as $imageURI) {
+        			echo "<li data-flip-title=\"\">
+			                <img src=\"".$imageURI."\" height='300'>
+			            </li>";
+        		}
+        	?>
+            
         </ul>
     </div>
 
@@ -240,49 +281,24 @@ foreach ($fbAlbums as $fbAlbum) {
     });
 </script>
 
-<pre class="code">$("#wheel").flipster({
-    style: 'wheel',
-    spacing: 0
-});</pre>
+
 </article>
 
 
 <article id="demo-flat" class="demo">
 
-    <h2>Flipster Flat</h2>
+    <h2></h2>
 
     <div id="flat">
         <ul>
-            <li data-flip-title="Red">
-                <img src="img/text1.gif">
-            </li>
-            <li data-flip-title="Razzmatazz" data-flip-category="Purples">
-                <img src="img/text2.gif">
-             </li>
-            <li data-flip-title="Deep Lilac" data-flip-category="Purples">
-                <img src="img/text3.gif">
-            </li>
-            <li data-flip-title="Daisy Bush" data-flip-category="Purples">
-                <img src="img/text4.gif">
-            </li>
-            <li data-flip-title="Cerulean Blue" data-flip-category="Blues">
-                <img src="img/text5.gif">
-            </li>
-            <li data-flip-title="Dodger Blue" data-flip-category="Blues">
-                <img src="img/text6.gif">
-            </li>
-            <li data-flip-title="Cyan" data-flip-category="Blues">
-                <img src="img/text7.gif">
-            </li>
-            <li data-flip-title="Robin's Egg" data-flip-category="Blues">
-                <img src="img/text8.gif">
-            </li>
-            <li data-flip-title="Deep Sea" data-flip-category="Greens">
-                <img src="img/text9.gif">
-            </li>
-            <li data-flip-title="Apple" data-flip-category="Greens">
-                <img src="img/text10.gif">
-            </li>
+            <?php
+        		foreach ($images as $imageURI) {
+        			echo "<li data-flip-title=\"\">
+			                <img src=\"".$imageURI."\" height='300'>
+			            </li>";
+        		}
+        	?>
+            
         </ul>
     </div>
 
@@ -292,12 +308,6 @@ foreach ($fbAlbums as $fbAlbum) {
         spacing: -0.25
     });
 </script>
-
-<pre class="code">$("#flat").flipster({
-    style: 'flat',
-    spacing: -0.25
-});</pre>
-
 </article>
 
 
