ee37c3d23ea4087470f571c61397a7286e5befdb
Tameesh Biswas <tameeshbiswas1998@gmail.com>
2018-02-02 17:12:35 +0530

    Model function call fix

--- new2018.php
+++ new2018.php
@@ -166,7 +166,7 @@
 <!--horizontal scrolling div-------->
 	<div class="horizontal">
 		<div id="leftnav" style="position:fixed;z-index:200;border-left:#000000;padding:  10px;border-radius: 0px 10px 0px 10px;border: solid #0f62bda6;;left:0px;top:50%;transform:translateY(-50%);">
-			<a class="ginputbox side " >Events</a><br>
+			<a class="ginputbox side " href="/events/">Events</a><br>
 			<a class="ginputbox side proside">Pro-Nites</a><br>
 			<a class="ginputbox side schside">Schedule</a><br>
 			<a class="ginputbox side guestside" >Guest Lectures</a><br>
