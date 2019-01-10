d3d0a7d1193803f1fe0c42f2898f60b3896bc7a4
Tameesh Biswas <tameeshbiswas1998@gmail.com>
2019-01-10 14:03:37 +0530

    Update reg.php

--- view/reg.php
+++ view/reg.php
@@ -697,6 +697,15 @@
 			</script>
 
 			
-		
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-90791019-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-90791019-1');
+</script>
+
 	</body>
 </html>
